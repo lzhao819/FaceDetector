21b2c1ba6b232a5bd9a94158604018a76d929d4f
Lu Zhao <lzhao819@usc.edu>
2020-08-31 23:31:25 -0700

    Updates

---
+++ precache-manifest.c2ae1d0746426049933d1f9e8ded5699.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53ef9a5d43f18d7d59352aa903349c3b",
+    "url": "/FaceDetector/index.html"
+  },
+  {
+    "revision": "d8bd24906a8d243464ef",
+    "url": "/FaceDetector/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "44ca7b5a8b7a579cd165",
+    "url": "/FaceDetector/static/css/main.d8309832.chunk.css"
+  },
+  {
+    "revision": "d8bd24906a8d243464ef",
+    "url": "/FaceDetector/static/js/2.2c121f0e.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/FaceDetector/static/js/2.2c121f0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44ca7b5a8b7a579cd165",
+    "url": "/FaceDetector/static/js/main.3cbd2e4e.chunk.js"
+  },
+  {
+    "revision": "9ec72041aa1349f8c45d",
+    "url": "/FaceDetector/static/js/runtime-main.1e443b1e.js"
+  }
+]);
